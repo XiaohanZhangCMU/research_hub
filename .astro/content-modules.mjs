935b34b5587ad84fa4f525dbddef014b23373912
Xiaohan Zhang <xiaohanzhang@Xiaohans-MBP.localdomain>
2026-08-07 09:32:58 -0700

    update

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,12 @@
 
 export default new Map([
+["src/content/posts/lit-review-agentic-llms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flit-review-agentic-llms.mdx&astroContentModuleFlag=true")],
+["src/content/posts/lit-review-distributed-training.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flit-review-distributed-training.mdx&astroContentModuleFlag=true")],
+["src/content/posts/lit-review-llm-architectures.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flit-review-llm-architectures.mdx&astroContentModuleFlag=true")],
+["src/content/posts/lit-review-llm-quantization.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flit-review-llm-quantization.mdx&astroContentModuleFlag=true")],
+["src/content/posts/lit-review-llm-fine-tuning.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flit-review-llm-fine-tuning.mdx&astroContentModuleFlag=true")],
+["src/content/posts/quantization-aware-distillation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fquantization-aware-distillation.mdx&astroContentModuleFlag=true")],
+["src/content/posts/qat-reading-list.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fqat-reading-list.mdx&astroContentModuleFlag=true")],
+["src/content/posts/lit-review-quantization-finetuned-llms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Flit-review-quantization-finetuned-llms.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/sde-mathematical-foundations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsde-mathematical-foundations.mdx&astroContentModuleFlag=true")]]);
 		
